309461acece2dca156496f16d6d5c4b01f833da0
izzaafkarina14 <122216304+izzaafkarina14@users.noreply.github.com>
2024-03-01 22:23:15 +0700

    Add files via upload

---
+++ esp to firebase part 2.ino
@@ -0,0 +1,77 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <Firebase_ESP_Client.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
+
+//Provide the token generation process info.
+#include "addons/TokenHelper.h"
+//Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
+// Insert your network credentials
+#define WIFI_SSID "POHARIN D177B_plus"
+#define WIFI_PASSWORD "AsampaiZ"
+
+// Insert Firebase project API Key
+#define API_KEY "AIzaSyDuop32d-oAtIt0ZlDLAmLJg0BvlHrDuxk"
+
+// Insert RTDB URLefine the RTDB URL */
+#define DATABASE_URL "https://bismillah-ambil-data-default-rtdb.asia-southeast1.firebasedatabase.app/" 
+
+//Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+bool signupOK = false;
+int mq4sensor=A0;
+int sensorvalue;
+String firestatus="";
+void setup(){
+  Serial.begin(9600);
+  pinMode(mq4sensor, INPUT); //MQ4 sensor
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Sign up */
+  if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+}
+
+void loop(){
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 5000 || sendDataPrevMillis == 0)){
+    sendDataPrevMillis = millis();
+    sensorvalue=analogRead(mq4sensor); /// read the MQ4 sensor
+    Serial.println(sensorvalue);
+    Firebase.RTDB.setFloat(&fbdo, "MQ4 Sensor/Value",sensorvalue);
+}
+}
